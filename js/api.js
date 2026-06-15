0d792570d3aea7c007b457bab11d48048f20454b
bernardo <bernardo@yardim.com.br>
2026-06-15 07:35:14 -0300

    Add functionality to save thoughts and improve UI rendering

--- js/api.js
+++ js/api.js
@@ -8,6 +8,22 @@ const api = {
       throw error;
     }
   },
+
+  async salvarPensamento(pensamento) {
+    try {
+      const resposta = await fetch("http://localhost:3000/pensamentos", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(pensamento),
+      });
+      return await resposta.json();
+    } catch (error) {
+      alert("Ops, algo deu errado.");
+      throw error;
+    }
+  },
 };
 
 export default api;
--- js/main.js
+++ js/main.js
@@ -1,5 +1,30 @@
 import ui from "./ui.js";
+import api from "./api.js";
 
 document.addEventListener("DOMContentLoaded", () => {
   ui.renderizarPensamentos();
+
+  const formulario = document.getElementById("pensamento-form");
+  const btnCancelarFormulario = document.getElementById("botao-cancelar");
+
+  formulario.addEventListener("submit", manipularEnvioFormulario);
+  btnCancelarFormulario.addEventListener("click", cancelarEnvioFormulario);
 });
+
+async function manipularEnvioFormulario(event) {
+  event.preventDefault();
+  const id = document.getElementById("pensamento-id").value;
+  const conteudo = document.getElementById("pensamento-conteudo").value;
+  const autoria = document.getElementById("pensamento-autoria").value;
+
+  try {
+    await api.salvarPensamento({ conteudo, autoria });
+    ui.renderizarPensamentos();
+  } catch {
+    alert("Ops, algo deu errado ao salvar o pensamento.");
+  }
+}
+
+function cancelarEnvioFormulario() {
+  ui.limparFormulario();
+}
